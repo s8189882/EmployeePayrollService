bb2782865fd93178351a7abace03602ec1ae372e
s8189882 <s8189882@gmail.com>
2022-09-27 10:59:13 +0530

    create master branch

---
+++ Employee_Payroll/src/main/java/com/java/io/Employee_Payroll/EmployeePayrollData.java
@@ -0,0 +1,20 @@
+package com.java.io.Employee_Payroll;
+
+public class EmployeePayrollData {
+	
+	public int id;
+	public String name;
+	public double salary;
+	
+	public EmployeePayrollData(int id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	@Override
+	public String toString() {
+		return "id : " + id + ", name : '" + name + "', salary : " + salary + "";
+	}
+}
